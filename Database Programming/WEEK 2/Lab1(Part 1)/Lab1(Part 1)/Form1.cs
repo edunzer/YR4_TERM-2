b0ae76406c865052893941a4e70de2391ad67932
Ethan Dunzer <ethandunzer@yahoo.com>
2021-01-15 11:18:36 -0800

    Week 2 database lab 1

---
+++ Database Programming/WEEK 2/Lab1(Part 1)/Lab1(Part 1)/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1_Part_1_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void productsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.productsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.tECHSUPPORTDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'tECHSUPPORTDataSet.Products' table. You can move, or remove it, as needed.
+            this.productsTableAdapter.Fill(this.tECHSUPPORTDataSet.Products);
+
+        }
+    }
+}
---
+++ Database Programming/WEEK 2/Lab1(Part 2)/Lab1(Part 2)/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1_Part_2_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void techniciansBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.techniciansBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.tECHSUPPORTDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'tECHSUPPORTDataSet.Technicians' table. You can move, or remove it, as needed.
+            this.techniciansTableAdapter.Fill(this.tECHSUPPORTDataSet.Technicians);
+
+        }
+    }
+}
